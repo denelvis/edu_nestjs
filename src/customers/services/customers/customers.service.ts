2137778c1046c86771942cd2731263b24412c741
Denis <denelvis@yandex.ru>
2022-02-26 00:59:54 +0300

    added some get requests

--- src/customers/services/customers/customers.service.ts
+++ src/customers/services/customers/customers.service.ts
@@ -2,7 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CustomersService {
-  findCustomer() {
-    return { id: 1, email: 'soccer@mail.com', createdAt: new Date() };
+  private users = [
+    {
+      id: 1,
+      email: 'soccer@mail.com',
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      email: 'pizza@mail.com',
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      email: 'hot@mail.com',
+      createdAt: new Date(),
+    },
+  ];
+
+  findCustomerById(id: number) {
+    return this.users.find((user) => user.id === id);
   }
 }

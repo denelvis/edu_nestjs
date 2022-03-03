b204868b84e57ea261c8741a37227517de3c39cd
Denis <denelvis@yandex.ru>
2022-03-03 23:16:01 +0300

    added typeorm model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomersModule } from './customers/customers.module';
 import { UsersModule } from './users/users.module';
+import entities from './typeorm';
 
 @Module({
-  imports: [CustomersModule, UsersModule],
+  imports: [
+    CustomersModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3333,
+      username: 'nestjs',
+      password: 'nestjs123',
+      database: 'nestjs_edu',
+      entities: entities,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
